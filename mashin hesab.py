8f0f70f866454cf29a421158e32af7137021ada5
unknown <fatemehazarshab895@gmail.com>
2022-04-22 11:21:42 +0430

    add file

---
+++ mashin hesab.py
@@ -0,0 +1,16 @@
+class mashinhesab():
+    def taqsimm(self,a,b):
+         print("taqsimm=",a/b)
+    def zarbadad(self,a,b):
+         print("zarbadad=",a*b)
+    def tafriq(self,a,b):
+         print("tafriq=",a-b)
+    def jam(self,a,b):
+        print("jam=",a+b)       
+    
+
+num_2=mashinhesab()
+num_2.taqsimm(4,8)
+num_2.zarbadad(4,8) 
+num_2.tafriq(4,8) 
+num_2.jam(4,8)   
